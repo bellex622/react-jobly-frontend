abac558388c71d0564d05d254b51351b56a569c4
Karly Stark <karlystark@gmail.com>
2023-08-29 16:54:42 -0700

    added routes, navigation, and api WIP part three

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter } from "react-router-dom";
+import Navigation from "./Navigation";
+import RoutesList from "./RoutesList";
 
 function App() {
   return (
     <div className="App">
-
-
+      <BrowserRouter>
+      <Navigation />
+      <RoutesList />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Navigation.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+
+function Navigation(){
+
+  let activeStyle= {
+    "fontWeight": "bold"
+  };
+
+  return(
+    <nav className="Navigation">
+      <NavLink to="/" className="Navigation-home" style={({ isActive }) =>
+      isActive ? activeStyle: undefined } end>
+      Jobly
+      </NavLink>
+
+      <NavLink to="/companies" className="Navigation-companies" style={({ isActive }) =>
+      isActive ? activeStyle: undefined } end>
+      Companies
+      </NavLink>
+
+      <NavLink to="/jobs" className="Navigation-jobs" style={({isActive }) =>
+      isActive? activeStyle: undefined } >
+      Jobs
+      </NavLink>
+    </nav>
+  )
+}
+
+export default Navigation;
